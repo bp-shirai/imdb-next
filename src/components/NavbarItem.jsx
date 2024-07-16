9f794462efa4ee1b2b66c84ea4b74a14d40ebe7a
bp.shirai <bp.shirai@gmail.com>
2024-07-17 04:29:33 +0900

    test

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -5,7 +5,5 @@ import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 
 export default function NavbarItem({ title, param }) {
-  const searchParams = useSearchParams();
-  const genre = searchParams.get("genre");
   return <div>test</div>;
 }
