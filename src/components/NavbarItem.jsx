db02b2f5b446fa38f1386dc20fdf4419dcf6d7b5
bp.shirai <bp.shirai@gmail.com>
2024-07-17 18:21:15 +0900

    the final update

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -5,9 +5,21 @@ import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 
 export default function NavbarItem({ title, param }) {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get("genre");
+  const isGenre = searchParams.has("genre");
   return (
     <div>
-      <Link href={`/?genre=${param}`}>{title}</Link>
+      <Link
+        className={`hover:text-amber-600 font-semibold ${
+          genre === param || (!isGenre && param === "fetchTrending")
+            ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
+            : ""
+        }`}
+        href={`/?genre=${param}`}
+      >
+        {title}
+      </Link>
     </div>
   );
 }
